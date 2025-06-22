df7a79ecd057e98720395a26a62f1467d9404876
Mohammad Mohitul Islam <mohitul.islam@technonext.com>
2025-06-22 13:21:40 +0600

    Create SQL scripts for multiple databases including invoicing, HR, inventory, and store
    - Added create-databases.sql to set up the sql_invoicing, sql_store, and sql_hr databases with necessary tables and initial data.
    - Created create-db-hr.sql for the HR database with offices and employees tables.
    - Introduced create-db-inventory.sql for the inventory database with products table.
    - Developed create-db-invoicing.sql for the invoicing database with payment methods, clients, invoices, and payments tables.
    - Established create-db-store.sql for the store database with products, shippers, customers, order statuses, orders, and order items tables.

---
+++ Inserting, Updating, and Deleting Data/column_attribute.sql
@@ -0,0 +1,10 @@
+
+SELECT * FROM salesdb.customers
+
+SELECT * from sql_hr.employees
+
+SELECT * FROM sql_inventory.products
+
+select * from sql_invoicing.clients
+
+SELECT * FROM sql_store.customers
