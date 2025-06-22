f89d54c0675c6d90c4c4149eed7c86f0c63438ad
Mohammad Mohitul Islam <mohitul.islam@technonext.com>
2025-06-22 13:51:25 +0600

    Add SQL script for inserting customer records into the sql_store database

---
+++ Inserting, Updating, and Deleting Data/inserting_row.sql
@@ -0,0 +1,31 @@
+use sql_store
+
+-- Check db schemas I mean fields variables types and then insert like that
+
+INSERT INTO
+    `customers`
+VALUES (
+        DEFAULT,
+        'Babara 2',
+        'MacCaffrey 4',
+        '1986-03-25',
+        '781-932-9754',
+        '0 Sage Terrace',
+        'Waltham',
+        'MA',
+        2273
+    );
+
+INSERT INTO
+    `customers`
+VALUES (
+        DEFAULT,
+        'Ines 3',
+        'Brushfield 4',
+        '1986-04-15',
+        '804-427-9456',
+        '14187 Commercial Trail',
+        'Hampton',
+        'VA',
+        947
+    );
